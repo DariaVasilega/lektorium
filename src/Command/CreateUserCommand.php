709b1372927f64653e5189ffc51025596eb0790e
DariaVasilega <dariavasilega@gmail.com>
2019-12-01 22:36:38 +0200

    create console command

---
+++ src/Command/CreateUserCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace App\Command;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CreateUserCommand extends Command
+{
+    // the name of the command (the part after "bin/console")
+    protected static $defaultName = 'app:create-user';
+    /**
+     * @var bool
+     */
+    private $requirePassword;
+
+    public function __construct(bool $requirePassword = false)
+    {
+        // best practices recommend to call the parent constructor first and
+        // then set your own properties. That wouldn't work in this case
+        // because configure() needs the properties set in this constructor
+        $this->requirePassword = $requirePassword;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this
+            // configure an argument
+            ->addArgument('username', InputArgument::REQUIRED, 'The username of the user.')
+            // ...
+        ;
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln([
+            'User Creator',
+            '============',
+            '',
+        ]);
+        $output->writeln('Username: '.$input->getArgument('username'));
+    }
+}
